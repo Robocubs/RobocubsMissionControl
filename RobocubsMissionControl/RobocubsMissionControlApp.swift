a8552a7ae77c926ea1387a073d48395a88fb3305
Quincy D <quincy.deklerk@outlook.com>
2025-07-14 15:18:32 -0400

    Initial Commit

---
+++ RobocubsMissionControl/RobocubsMissionControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  RobocubsMissionControlApp.swift
+//  RobocubsMissionControl
+//
+//  Created by Quincy D on 7/14/25.
+//
+
+import SwiftUI
+
+@main
+struct RobocubsMissionControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
