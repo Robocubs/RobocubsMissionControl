0042f6591609a71d0fa6c53c395fe1a185b5f023
Quincy D <quincy.deklerk@outlook.com>
2025-08-02 11:07:20 -0400

    Run uvicorn in main thread

--- CartInformationDisplays/main.py
+++ CartInformationDisplays/main.py
@@ -3,16 +3,12 @@ import uvicorn
 from communicationEngine import cartLSend, app
 import threading
 
-def startServer():
-    uvicorn.run(app, host="0.0.0.0", port=8010)
-
-def main():
+def cli_loop():
     while True:
         userInput = input("Enter something: ")
         asyncio.run(cartLSend({"type": "state", "data": userInput}))
 
-
 if __name__ == "__main__":
-    serverThread = threading.Thread(target=startServer)
-    serverThread.start()
-    main()
+    cliThread = threading.Thread(target=cli_loop, daemon=True)
+    cliThread.start()
+    uvicorn.run(app, host="0.0.0.0", port=8010)
