7dac9ab5c619ad1741e78f54464ae4863b0e0993
Quincy D <quincy.deklerk@outlook.com>
2025-08-02 12:26:59 -0400

    Fixed YouTube viewer

--- CartInformationDisplays/frontend/src/views/YouTube.svelte
+++ CartInformationDisplays/frontend/src/views/YouTube.svelte
@@ -7,5 +7,7 @@
     src={"https://www.youtube-nocookie.com/embed/" + videoID + "?controls=0&autoplay=1&mute=1"}
     height="100%"
     width="100%"
+    frameborder="0"
+    scrolling="no"
     allowfullscreen>
 </iframe>
