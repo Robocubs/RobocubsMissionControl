596b5fd755572da4f698fe20a20c21d1bdc87c9e
Quincy D <quincy.deklerk@outlook.com>
2025-08-02 08:39:05 -0400

    Updated slides transition time and build

--- CartInformationDisplays/frontend/prod/assets/left-ys2pUwfy.js
+++
@@ -1,1 +0,0 @@
-import{m as t,L as e}from"./Left-BEHgRf_g.js";t(e,{target:document.getElementById("left")});
---
+++ CartInformationDisplays/frontend/prod/assets/right-B1ApUhCd.js
@@ -0,0 +1,1 @@
+import{m as t,L as e}from"./Left-Dpc5nprV.js";t(e,{target:document.getElementById("left")});
