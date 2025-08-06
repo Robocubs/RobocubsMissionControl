e9b86464cc7879027e3402e363a39a417e74b692
tjkr0wn <joumaa_family@icloud.com>
2025-08-06 17:00:10 -0400

    Added thread to send data to master iPad

--- CartInformationDisplays/main.py
+++ CartInformationDisplays/main.py
@@ -2,6 +2,19 @@ import asyncio
 import uvicorn
 import threading
 from app import app
+from tba import getMatches
+from communicationBus import communicationBus
+import time
+
+def threadMasterMatchDataSender():
+    while True:
+        time.sleep(10)
+        matches = getMatches()
+        if matches != []:
+            communicationBus.sendMissionController(matches)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=1701)
+    masterMatchDataThread = threading.Thread(target=threadMasterMatchDataSender
+                                       )
+    masterMatchDataThread.start()
