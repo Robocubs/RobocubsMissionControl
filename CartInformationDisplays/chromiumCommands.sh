48e36c2eb822738b6c81479a41046313d8367c5c
Quincy D <quincy.deklerk@outlook.com>
2025-08-06 14:19:44 -0400

    Updated chromium shell script

--- CartInformationDisplays/chromiumCommands.sh
+++ CartInformationDisplays/chromiumCommands.sh
@@ -1,2 +1,8 @@
+sleep 5
+
+# Set display and auth environment
+export DISPLAY=:0
+export XAUTHORITY=/home/pi/.Xauthority
+
 chromium-browser --window-position=3840,0 --kiosk https://www.apple.com
 chromium-browser --window-position=0,0 --kiosk https://www.apple.com
