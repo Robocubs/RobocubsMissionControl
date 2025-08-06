20ef3223a5b69a48aeb8e2776739f17b72b98d73
Quincy D <quincy.deklerk@outlook.com>
2025-08-06 15:47:36 -0400

    Fixed booting dual chrome windows

--- CartInformationDisplays/chromiumCommands.sh
+++ CartInformationDisplays/chromiumCommands.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
 sleep 5
 
 # Set display and auth environment
 export DISPLAY=:0
 export XAUTHORITY=/home/pi/.Xauthority
 
-chromium-browser --window-position=3840,0 --kiosk https://www.apple.com
-chromium-browser --window-position=0,0 --kiosk https://www.apple.com
+chromium-browser --window-position=3840,0 --kiosk --user-data-dir=/tmp/chrome1 "https://www.apple.com" &
+chromium-browser --window-position=0,0 --kiosk --user-data-dir=/tmp/chrome2 "https://www.apple.com" &
+
+wait
