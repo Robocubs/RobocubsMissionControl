07bda91243112bb55eba6bd9d64829ed6a7a9563
Quincy D <quincy.deklerk@outlook.com>
2025-08-09 09:47:41 -0400

    Fixed twitch view

--- CartInformationDisplays/frontend/src/views/Twitch.svelte
+++ CartInformationDisplays/frontend/src/views/Twitch.svelte
@@ -7,5 +7,7 @@
     src={"https://player.twitch.tv/?channel=" + channel + "&parent=localhost&autoplay=true&muted=true&controls=false"}
     height="100%"
     width="100%"
+    frameborder="0"
+    scrolling="no"
     allowfullscreen>
 </iframe>
