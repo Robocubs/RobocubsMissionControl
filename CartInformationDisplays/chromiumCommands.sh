959e36b26692c807d33f05d5db5d5f4060c85731
Quincy D <quincy.deklerk@outlook.com>
2025-08-11 18:07:04 -0400

    Updated start commands

--- CartInformationDisplays/chromiumCommands.sh
+++ CartInformationDisplays/chromiumCommands.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 sleep 5
 
-# Set display and auth environment
-export DISPLAY=:0
-export XAUTHORITY=/home/pi/.Xauthority
+# Launch on workspace 1 (left)
+swaymsg "workspace 1"
+chromium-browser --kiosk --user-data-dir="/tmp/chrome2" "http://localhost:1701/prod/left.html" &
 
-chromium-browser --window-position=3840,0 --kiosk --user-data-dir="/tmp/chrome1" "http://localhost:1701/prod/right.html" &
-chromium-browser --window-position=0,0 --kiosk --user-data-dir="/tmp/chrome2" "http://localhost:1701/prod/left.html" &
+# Launch on workspace 2 (right)
+swaymsg "workspace 2"
+chromium-browser --kiosk --user-data-dir="/tmp/chrome1" "http://localhost:1701/prod/right.html" &
 
 wait
