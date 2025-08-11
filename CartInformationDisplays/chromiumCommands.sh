4ca9b2e839deba7002803b887cda20ce9e6db5dc
Quincy D <quincy.deklerk@outlook.com>
2025-08-11 14:45:02 -0400

    Undo force graphics acceleration

--- CartInformationDisplays/chromiumCommands.sh
+++ CartInformationDisplays/chromiumCommands.sh
@@ -5,7 +5,7 @@ sleep 5
 export DISPLAY=:0
 export XAUTHORITY=/home/pi/.Xauthority
 
-chromium-browser --enable-gpu-rasterization --enable-zero-copy --ignore-gpu-blocklist --enable-features=VaapiVideoDecoder --use-gl=egl --window-position=3840,0 --kiosk --user-data-dir="/tmp/chrome1" "http://localhost:1701/prod/right.html" &
-chromium-browser --enable-gpu-rasterization --enable-zero-copy --ignore-gpu-blocklist --enable-features=VaapiVideoDecoder --use-gl=egl --window-position=0,0 --kiosk --user-data-dir="/tmp/chrome2" "http://localhost:1701/prod/left.html" &
+chromium-browser --window-position=3840,0 --kiosk --user-data-dir="/tmp/chrome1" "http://localhost:1701/prod/right.html" &
+chromium-browser --window-position=0,0 --kiosk --user-data-dir="/tmp/chrome2" "http://localhost:1701/prod/left.html" &
 
 wait
