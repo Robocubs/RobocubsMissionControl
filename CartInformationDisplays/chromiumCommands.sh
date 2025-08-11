7fbb9960dc883d41e57e8a227d5c5834ad4aee15
Quincy D <quincy.deklerk@outlook.com>
2025-08-11 18:14:35 -0400

    Updated start script

--- CartInformationDisplays/chromiumCommands.sh
+++ CartInformationDisplays/chromiumCommands.sh
@@ -3,10 +3,14 @@ sleep 5
 
 # Launch on workspace 1 (left)
 swaymsg "workspace 1"
+sleep 0.5
 chromium-browser --kiosk --user-data-dir="/tmp/chrome2" "http://localhost:1701/prod/left.html" &
+
+sleep 0.5
 
 # Launch on workspace 2 (right)
 swaymsg "workspace 2"
+sleep 0.5
 chromium-browser --kiosk --user-data-dir="/tmp/chrome1" "http://localhost:1701/prod/right.html" &
 
 wait
